2945caa71018e8f704fcb76f206559e21e0d3418
Tony Pun <tony@Tonys-MacBook-Pro.local>
2018-04-14 16:04:36 -0400

    step-4 - Adding flip animations and word definition fragments

---
+++ app/src/main/java/com/refactored/satvocabrefactored/DefinitionFragment.java
@@ -0,0 +1,20 @@
+package com.refactored.satvocabrefactored;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DefinitionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View defView = inflater.inflate(R.layout.definition_fragment, container, false);
+        TextView defTextView = (TextView) defView.findViewById(R.id.defTextView);
+        defTextView.setText("Test Definition");
+
+        return defView;
+    }
+}
--- app/src/main/java/com/refactored/satvocabrefactored/WordActivity.java
+++ app/src/main/java/com/refactored/satvocabrefactored/WordActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ViewAnimator;
 
 public class WordActivity extends AppCompatActivity {
+    
+    ViewAnimator viewAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +18,21 @@ public class WordActivity extends AppCompatActivity {
 
         WordFragment wordFragment = new WordFragment();
 
+        DefinitionFragment wordDefFragment = new DefinitionFragment();
+
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         ft.add(R.id.wordNameContainer, new WordFragment());
+        ft.add(R.id.wordDefContainer, new DefinitionFragment());
         ft.commit();
+
+        viewAnimator = (ViewAnimator) findViewById(R.id.viewFlipper);
+
+        viewAnimator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AnimationFactory.flipTransition(viewAnimator, AnimationFactory.FlipDirection.LEFT_RIGHT, 50);
+            }
+        });
     }
 }

9f08522b818ee667571b0d0419aa2407119198f4
Tony Pun <tony@Tonys-MacBook-Pro.local>
2018-04-14 15:16:41 -0400

    step-2 - Load words from JSON file to memory; Change layout components

--- app/src/main/java/com/refactored/satvocabrefactored/MainActivity.java
+++ app/src/main/java/com/refactored/satvocabrefactored/MainActivity.java
@@ -1,13 +1,52 @@
 package com.refactored.satvocabrefactored;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String fileName = "majortests_words.json";
+    private JSONArray wordBankAry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadWords();
+    }
+
+    public String loadJSONData() {
+        String jsonStr = null;
+        try {
+            InputStream iStream = getAssets().open(fileName);
+            int size = iStream.available();
+            byte[] buffer = new byte[size];
+            iStream.read(buffer);
+            iStream.close();
+
+            jsonStr = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return jsonStr;
+    }
+
+    public void loadWords() {
+        try {
+            String jsonStr = loadJSONData();
+            JSONObject jsonObj = new JSONObject(jsonStr);
+            wordBankAry = jsonObj.getJSONArray("wordBank");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
